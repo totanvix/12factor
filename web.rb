736a986abdce722e1ab263ea0c62ad55fea7c376
Johnathan Lyman <jelyman@gmail.com>
2024-02-28 12:40:43 -0800

    Correct locale loading (#355)
    * Correct locale loading for Ruby 3.3
    
    * Add current directory
    
    * Remove bundle exec

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ require 'rack/ssl-enforcer'
 configure do
   use Rack::SslEnforcer if ENV['FORCE_SSL']
   I18n.enforce_available_locales = true
-  I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.load_path = Dir[File.join(File.dirname(__FILE__), 'locales', '*.yml')]
   I18n.backend.load_translations
   I18n.default_locale = :en
 end
